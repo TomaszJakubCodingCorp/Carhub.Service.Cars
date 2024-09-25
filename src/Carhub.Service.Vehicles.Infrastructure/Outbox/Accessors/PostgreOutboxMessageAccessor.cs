c33470ba29d7794dbd5a29569c7949fc15b0f861
Tomasz Krasieńko <94125393+TomaszKrasienko@users.noreply.github.com>
2024-09-25 16:30:27 +0200

    Filled message accessor in outbox

--- src/Carhub.Service.Vehicles.Infrastructure/Outbox/Accessors/PostgreOutboxMessageAccessor.cs
+++ src/Carhub.Service.Vehicles.Infrastructure/Outbox/Accessors/PostgreOutboxMessageAccessor.cs
@@ -1,17 +1,55 @@
+using System.Collections.Concurrent;
 using Carhub.Service.Vehicles.Infrastructure.Outbox.Accessors.Abstractions;
+using Carhub.Service.Vehicles.Infrastructure.Outbox.Configuration;
 using Carhub.Service.Vehicles.Infrastructure.Outbox.Models;
+using Microsoft.Extensions.Options;
 
 namespace Carhub.Service.Vehicles.Infrastructure.Outbox.Accessors;
 
-internal sealed class PostgreOutboxMessageAccessor : IOutboxMessageAccessor
+internal sealed class InMemoryOutboxMessageAccessor : IOutboxMessageAccessor
 {
+    private readonly ConcurrentDictionary<string, OutboxMessage> _outboxMessages = [];
+    private readonly TimeSpan _expiry;
+
+    public InMemoryOutboxMessageAccessor(IOptions<OutboxOptions> options)
+    {
+        _expiry = options.Value.Expiry;
+    }
+    
     public Task<List<OutboxMessage>> GetUnsentAsync()
     {
-        throw new NotImplementedException();
+        return Task.FromResult(_outboxMessages
+            .Where(x => x.Value.ProcessedAt is null)
+            .Select(x => x.Value)
+            .ToList());
     }
 
     public Task ProcessAsync(OutboxMessage outboxMessage)
     {
-        throw new NotImplementedException();
+        outboxMessage.ProcessedAt = DateTime.Now;
+        RemoveExpiredMessages();
+        return Task.CompletedTask;
     }
+
+    private void RemoveExpiredMessages()
+    {
+        if (_expiry == TimeSpan.Zero)
+        {
+            return;
+        }
+
+        var now = DateTime.Now;
+        var expiredMessages = _outboxMessages
+            .Where(x => CountMessageLifeTime(now, x.Value) >= _expiry)
+            .ToDictionary();
+        foreach (var expiredMessage in expiredMessages)
+        {
+            _outboxMessages.TryRemove(expiredMessage.Key, out _);
+        }
+    }
+
+    private static TimeSpan CountMessageLifeTime(DateTime now, OutboxMessage outboxMessage)
+        => outboxMessage.ProcessedAt is null ? 
+            TimeSpan.Zero : 
+            TimeSpan.FromMicroseconds(now.Microsecond - outboxMessage.ProcessedAt.Value.Microsecond);
 }
--- src/Carhub.Service.Vehicles.Infrastructure/Outbox/Configuration/OutboxOptions.cs
+++ src/Carhub.Service.Vehicles.Infrastructure/Outbox/Configuration/OutboxOptions.cs
@@ -3,6 +3,6 @@ namespace Carhub.Service.Vehicles.Infrastructure.Outbox.Configuration;
 public sealed record OutboxOptions
 {
     public bool Enabled { get; init; }
-    public string ConnectionString { get; init; }
     public TimeSpan Interval { get; set; }
+    public TimeSpan Expiry { get; set; }
 }
